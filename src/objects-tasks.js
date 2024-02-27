d4f2a506cb53a84463ecb44e9154d70f1e88de77
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-02-27 21:32:27 +0300

    feat: add makeWord func

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -127,8 +127,19 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  return new Array(lettersObject)
+    .reduce((acc, curr) => {
+      const entries = Object.entries(curr);
+      entries.forEach((parameter) => {
+        const indexes = parameter[1];
+        indexes.forEach((index) => {
+          [acc[index]] = parameter;
+        });
+      });
+      return acc;
+    }, [])
+    .join('');
 }
 
 /**
